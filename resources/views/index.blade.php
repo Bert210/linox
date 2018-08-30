f250554b5291464d550c261ece25589cf471fe2d
Robert <bert210@gmail.com>
2018-08-29 21:39:58 -0400

    Created the login screen

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ app()->getLocale() }}" class="h-100">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,6 +11,28 @@
 
         <title>Linox</title>
     </head>
-    <body>
+    <body class="h-100">
+        <div class="container-fluid d-flex h-100 justify-content-center align-items-center">
+            <div class="card w-25">
+                <div class="card-header text-center font-weight-bold"><h3>Login</h3></div>
+                <div class="card-body">
+                    <form action="">
+
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
